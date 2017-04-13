d9362d02aeaea3739bbe442f9da12e86b68a00c9
alexbeyd <beyderman.alex@gmail.com>
2017-04-13 14:44:25 -0500

    added pdf documentation

---
+++ theUnity/MethodsLib.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theUnity
+{
+    public interface IMethodsLib
+    {
+        string Method1();
+        void Method2();
+    }
+
+    public class MethodsLib : IMethodsLib
+    {
+        public string Method1()
+        {
+            Method2();
+            return "Method1 finished";
+        }
+
+        public void Method2()
+        {
+
+        }
+    }
+}
