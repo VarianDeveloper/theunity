fe33007d01dab601afcf1636cb7c7cabfe06c3fb
alexbeyd <beyderman.alex@gmail.com>
2017-04-14 17:17:10 -0500

    cleanup

---
+++ theUnity/LoggingAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace theUnity
+{
+    public class LoggingAttribute : HandlerAttribute
+    {
+        private readonly int order;
+
+        public LoggingAttribute()
+        {
+            this.order = 1;
+        }
+
+        public override ICallHandler CreateHandler(IUnityContainer container)
+        {
+            return new LoggingCallHandler() {Order = order};
+        }
+    }
+}
---
+++ theUnity/LoggingInterseptionBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace theUnity
+{
+    class LoggingCallHandler : ICallHandler //IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public bool WillExecute { get { return true; } }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextHandlerDelegate getNext)
+        {
+            var args = input.Arguments.Count == 0 ? null : input.Arguments[0];
+
+            Console.WriteLine(
+                            $"Start: Invoking method: {input.MethodBase} with parameters \"{args}\" " +
+                            $"at {DateTime.Now.ToLongTimeString()}");
+
+            var result = getNext()(input, getNext);
+
+            Console.WriteLine(
+                $"Method {input.MethodBase} returned \"{result.ReturnValue}\" " +
+                              $"at {DateTime.Now.ToLongTimeString()}");
+
+            return result;
+        }
+
+        public int Order { get; set; }
+    }
+}
